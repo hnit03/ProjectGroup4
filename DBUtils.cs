8b07025e2df224c7f3049e0eab6256dd7d7d0a28
Nhinh <hoangnhinguyen33@gmail.com>
2021-03-07 21:24:52 +0700

    class connect db

---
+++ DBUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ProjectGroup4
+{
+    class DBUtils
+    {
+        public static SqlConnection GetSqlConnection()
+        {
+            String cnStr = "Data Source=SE141072;Initial Catalog=QLSVien;User ID=sa;Password=hoangnhi";
+            SqlConnection sqlConnection = new SqlConnection(cnStr);
+            return sqlConnection;
+        }
+    }
+}
